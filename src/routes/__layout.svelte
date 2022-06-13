c257914ab82921ee3cc79e20a995cef12a8dc147
Sergey Kolbasov <sergey.kolbasov@zalando.fi>
2022-06-13 15:57:05 +0300

    Setting up a project

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<div class="grid grid-cols-6 h-screen">
+	<div class="p-4 border-r-indigo-50 border-r-2">01</div>
+	<div class="col-span-3 p-4 border-r-indigo-50 border-r-2">02</div>
+	<div class="col-span-2 p-4">03</div>
+</div>
